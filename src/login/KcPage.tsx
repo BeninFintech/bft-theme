6af5a1193a973a56b76ef164c92115677335ba74
paulgrammer <paulmugaya@live.com>
2025-12-08 08:43:17 +0100

    feat: add UpdateEmail page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -37,6 +37,7 @@ const LoginPageExpired = lazy(() => import("./pages/LoginPageExpired"));
 const LoginConfigTotp = lazy(() => import("./pages/LoginConfigTotp"));
 const LogoutConfirm = lazy(() => import("./pages/LogoutConfirm"));
 const IdpReviewUserProfile = lazy(() => import("./pages/IdpReviewUserProfile"));
+const UpdateEmail = lazy(() => import("./pages/UpdateEmail"));
 const Error = lazy(() => import("./pages/Error"));
 const Info = lazy(() => import("./pages/Info"));
 const Terms = lazy(() => import("./pages/Terms"));
@@ -226,6 +227,16 @@ export default function KcPage(props: { kcContext: KcContext }) {
           case "idp-review-user-profile.ftl":
             return (
               <IdpReviewUserProfile
+                {...{ kcContext, i18n, classes }}
+                Template={LoginTemplate}
+                doUseDefaultCss={false}
+                UserProfileFormFields={CustomUserProfileFormFields}
+                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+              />
+            );
+          case "update-email.ftl":
+            return (
+              <UpdateEmail
                 {...{ kcContext, i18n, classes }}
                 Template={LoginTemplate}
                 doUseDefaultCss={false}
